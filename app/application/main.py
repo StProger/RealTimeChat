48ef27e0c51e899dd184179425627ea9d9d7abfd
Kirill Mikhailov <kicode@mail.ru>
2024-12-16 00:29:35 +0300

    initial commit

---
+++ app/application/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+def create_app():
+    return FastAPI(
+        title="FastAPI + Kafka + Websockets Chat App",
+        docs_url="/api/docs",
+        description="A simple Kafka + DDD chat"
+    )

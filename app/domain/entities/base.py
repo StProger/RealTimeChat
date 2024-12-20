451649b99cabcbffeefe7d41afa6dd63bb1371da
Kirill Mikhailov <kicode@mail.ru>
2024-12-21 01:19:45 +0300

    events

--- app/domain/entities/base.py
+++ app/domain/entities/base.py
@@ -1,19 +1,30 @@
 import uuid
 from abc import ABC
+from copy import copy
 from dataclasses import dataclass, field
 
 import datetime
 
+from domain.events.base import BaseEvent
+
 
 @dataclass
-class BaseEntity:
+class BaseEntity(ABC):
     oid: str = field(default_factory=lambda: str(uuid.uuid4()),
                      kw_only=True)
-    created_at: datetime.datetime = field(default_factory=datetime.datetime.now,
-                                          kw_only=True)
+    _events: list[BaseEvent] = field(default_factory=list, kw_only=True)
 
-    def __hash__(self) -> int:
-        return hash(self.oid)
+    def register_event(self, event: BaseEvent) -> None:
+        self._events.append(event)
 
-    def __eq__(self, __value: 'BaseEntity') -> bool:
-        return self.oid == __value.oid
+    def pull_events(self) -> list[BaseEvent]:
+        registered_events = copy(self._events)
+        self._events.clear()
+
+        return registered_events
+
+    # def __hash__(self) -> int:
+    #     return hash(self.oid)
+    #
+    # def __eq__(self, __value: 'BaseEntity') -> bool:
+    #     return self.oid == __value.oid
--- app/domain/entities/messages.py
+++ app/domain/entities/messages.py
@@ -8,34 +8,32 @@ from domain.values.messages import Text, Title
 
 
 @dataclass
-class Message:
+class Message(BaseEntity):
     text: Text
-    oid: str = field(default_factory=lambda: str(uuid.uuid4()),
-                     kw_only=True)
+
     created_at: datetime.datetime = field(default_factory=datetime.datetime.now,
                                           kw_only=True)
 
     def __hash__(self) -> int:
         return hash(self.oid)
 
-    def __eq__(self, __value: 'BaseEntity') -> bool:
+    def __eq__(self, __value: 'Message') -> bool:
         return self.oid == __value.oid
 
 
 @dataclass
-class Chat:
+class Chat(BaseEntity):
     title: Title
     messages: set[Message] = field(default_factory=set,
                                    kw_only=True)
-    oid: str = field(default_factory=lambda: str(uuid.uuid4()),
-                     kw_only=True)
+
     created_at: datetime.datetime = field(default_factory=datetime.datetime.now,
                                           kw_only=True)
 
     def __hash__(self) -> int:
         return hash(self.oid)
 
-    def __eq__(self, __value: 'BaseEntity') -> bool:
+    def __eq__(self, __value: 'Chat') -> bool:
         return self.oid == __value.oid
 
     def add_message(self, message: Message):
